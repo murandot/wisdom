9f2f3aa46734180adcb779fb0015dfcc0e3fcd4b
h.murayama <103624383+murandot@users.noreply.github.com>
2022-05-17 14:24:32 +0900

    デプロイの準備

---
+++ db/migrate/20220517032643_create_materials.rb
@@ -0,0 +1,13 @@
+class CreateMaterials < ActiveRecord::Migration[6.0]
+  def change
+    create_table :materials do |t|
+      t.string     :title,              null: false
+      t.text       :content,            null: false
+      t.integer    :school_category_id, null: false
+      t.integer    :subject_id,         null: false
+      t.integer    :grade_id,           null: false
+      t.references :user,               null: false
+      t.timestamps
+    end
+  end
+end
